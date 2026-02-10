833abdf997aae2698cba3c301c498646412e98b8
karanbhatt1 <karanbhatt759@gmail.com>
2026-02-10 13:34:45 +0530

    ReplaceAllSpaceToa

---
+++ src/String/ReplaceAllSpacetoa.java
@@ -0,0 +1,9 @@
+package String;
+
+public class ReplaceAllSpacetoa {
+    public static void main(String[] args) {
+        String s  = "I am trying to be a good man";
+        s= s.replaceAll(" ","a");
+        System.out.println(s);
+    }
+}
